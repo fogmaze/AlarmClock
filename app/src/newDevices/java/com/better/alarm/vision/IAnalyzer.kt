1222e28d32b72f0758dbef3d7a25d5e57204a9bc
andy <andy16830@gmail.com>
2025-06-13 21:35:16 +0800

    save peek result into picture

--- app/src/newDevices/java/com/better/alarm/vision/IAnalyzer.kt
+++ app/src/newDevices/java/com/better/alarm/vision/IAnalyzer.kt
@@ -1,8 +1,11 @@
 package com.better.alarm.vision
 
+import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Matrix
+import android.os.Build
+import android.provider.MediaStore
 import androidx.camera.core.ImageAnalysis
 import com.better.alarm.bootstrap.globalLogger
 import com.google.mediapipe.framework.image.BitmapImageBuilder
@@ -21,6 +24,9 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.math.sqrt
@@ -85,6 +91,10 @@ class IAnalyzer (
     }
   }
 
+  // debug
+  private var isPeekFinish = false
+  private var peekResult = STATUS_UNKNOWN
+
   init {
     headDetectorV10 = DetectorV10(context, HEAD_MODEL_PATH, HeadDetectorListener(), {
         logger.debug { it }
@@ -130,6 +140,12 @@ class IAnalyzer (
       bitmapBuffer, 0, 0, bitmapBuffer.width, bitmapBuffer.height,
       matrix, true
     )
+    // debug
+    if(isPeekFinish) {
+      savePeekResultToGallery(rotatedBitmap, peekResult)
+      isPeekFinish = false
+    }
+
     val mpImage = BitmapImageBuilder(rotatedBitmap).build()
     synchronized(gestureRecognizerOperatingLock) {
       gestureRecognizer?.detectAsync(mpImage, System.currentTimeMillis())
@@ -167,9 +183,13 @@ class IAnalyzer (
         if (it == STATUS_DETECTED) {
           logger.debug { "onDetectAction" }
           onDetectAction()
+          peekResult = STATUS_DETECTED
+          isPeekFinish = true
         } else {
           logger.debug { "onTimeoutAction" }
           onTimeoutAction()
+          peekResult = STATUS_NOT_DETECTED
+          isPeekFinish = true
         }
       }
       compositeDisposable.add(disposable)
@@ -314,5 +334,33 @@ class IAnalyzer (
     headDetectorV10 = null
     headDetectorExecutor.shutdown()
     compositeDisposable.dispose()
+  }
+
+  private fun savePeekResultToGallery(bitmap: Bitmap, result: Int) {
+    val displayName = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.TAIWAN).format(System.currentTimeMillis()) + "_${result == STATUS_DETECTED}"
+    val contentValues = ContentValues().apply {
+      put(MediaStore.Images.Media.DISPLAY_NAME, "$displayName.jpg")
+      put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+      put(MediaStore.Images.Media.WIDTH, bitmap.width)
+      put(MediaStore.Images.Media.HEIGHT, bitmap.height)
+      put(MediaStore.Images.Media.IS_PENDING, 1)
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/AlarmClock")
+      }
+    }
+
+    val imageUri = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+    imageUri?.let { uri ->
+      context.contentResolver.openOutputStream(uri)?.use { outputStream ->
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+          contentValues.clear()
+          contentValues.put(MediaStore.Images.Media.IS_PENDING, 0)
+          context.contentResolver.update(uri, contentValues, null, null)
+        }
+      }
+    }
   }
 }
